9517d5580cd6044c7b6212ba528815cba898cd26
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-20 19:10:16 -0300

    Sobrescritura y reforma de método saludar con tolerancia de espacios extremos

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,6 +1,8 @@
 package loom.hola_mundo;
 
 /*
+ * @author lunavi97
+ * 
  * Devuelve el saludo a una persona en particular
  * o de manera genérica si no se indica
  */
@@ -10,10 +12,26 @@ public class HolaMundo {
     public String saludar(final String persona) {
     	
     	/*
+    	 * Saludar
+    	 * 
     	 * @param persona: nombre de la persona a saludar
     	 * @return mensaje de saludo
     	 */
     	
-        return persona != "" ? "¡Hola, " + persona + "!" : "¡Hola!";
+        if(persona == "") {
+        	return "¡Hola!";
+        } else {
+        	return "¡Hola, " + persona.trim() + "!";
+        }
+    }
+    
+    public String saludar() {
+    	/*
+    	 * Saludar
+    	 * 
+    	 * @return saludo genérico
+    	 */
+    	
+    	return "¡Hola!";
     }
 }
--- src/test/java/loom/hola_mundo/HolaMundoTests.java
+++ src/test/java/loom/hola_mundo/HolaMundoTests.java
@@ -1,18 +1,35 @@
 package loom.hola_mundo;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HolaMundoTests {
+	
+	HolaMundo holaMundo;
+	
+	@Before
+	public void inicialize() {
+		holaMundo = new HolaMundo();
+	}
+	
     @Test
     public void queSaludaCorrectamente() {
-        HolaMundo holaMundo = new HolaMundo();
         Assert.assertEquals("¡Hola, Kent Beck!", holaMundo.saludar("Kent Beck"));
     }
     
     @Test
-    public void sinPersona() {
-    	HolaMundo holaMundo = new HolaMundo();
+    public void personaVacia() {
     	Assert.assertEquals("¡Hola!", holaMundo.saludar(""));
+    }
+    
+    @Test
+    public void sinPersona() {
+    	Assert.assertEquals("¡Hola!", holaMundo.saludar());
+    }
+    
+    @Test
+    public void personaConEspaciosExtremos() {
+    	Assert.assertEquals("¡Hola, Luis Martinez!", holaMundo.saludar(" Luis Martinez "));
     }
 }
