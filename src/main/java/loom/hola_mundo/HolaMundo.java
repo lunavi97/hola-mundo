f8ec0c8b7df4ad36976794687ccaa7226bb60e0f
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-21 15:13:49 -0300

    Reforma de comentarios y uso de operador condicional ternario

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -2,36 +2,26 @@ package loom.hola_mundo;
 
 /*
  * @author lunavi97
- * 
- * Devuelve el saludo a una persona en particular
- * o de manera genérica si no se indica
+ * Devuelve el saludo a una persona en particular o de manera genérica si no se indica.
  */
-
 public class HolaMundo {
 
-    public String saludar(final String persona) {
-    	
-    	/*
-    	 * Saludar
-    	 * 
-    	 * @param persona: nombre de la persona a saludar
-    	 * @return mensaje de saludo
-    	 */
-    	
-        if(persona == "") {
-        	return "¡Hola!";
-        } else {
-        	return "¡Hola, " + persona.trim() + "!";
-        }
-    }
-    
-    public String saludar() {
-    	/*
-    	 * Saludar
-    	 * 
-    	 * @return saludo genérico
-    	 */
-    	
-    	return "¡Hola!";
-    }
+	/*
+	 * Saludar.
+	 * 
+	 * @param nombre de la persona a saludar.
+	 * @return mensaje de saludo.
+	 */
+	public String saludar(final String persona) {
+		return persona == "" ? "¡Hola!" : "¡Hola, " + persona.trim() + "!";
+	}
+
+	/*
+	 * Saludar.
+	 * 
+	 * @return saludo genérico.
+	 */
+	public String saludar() {
+		return "¡Hola!";
+	}
 }
