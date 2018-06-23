3cb1b1db83f8825143c5a86694998ba2a81689e6
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-23 16:13:32 -0300

    Formateo de texto

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -7,24 +7,24 @@ package loom.hola_mundo;
  * de manera genérica si no se indica.
  */
 public class HolaMundo {
-	/**
-	 * Saludar.
-	 * @param nombre de la persona a saludar.
-	 * @return mensaje de saludo.
-	 */
-	public String saludar(final String persona) {
-		if(persona == "") {
-			return "¡Hola!";
-		} else {
-			return "¡Hola, " + persona.trim() + "!";
-		}
-	}
+    /**
+     * Saludar.
+     * @param nombre de la persona a saludar.
+     * @return mensaje de saludo.
+     */
+    public String saludar(final String persona) {
+        if (persona == "") {
+            return "¡Hola!";
+        } else {
+            return "¡Hola, " + persona.trim() + "!";
+        }
+    }
 
-	/**
-	 * Saludar.
-	 * @return saludo genérico.
-	 */
-	public String saludar() {
-		return "¡Hola!";
-	}
+    /**
+     * Saludar.
+     * @return saludo genérico.
+     */
+    public String saludar() {
+        return "¡Hola!";
+    }
 }
