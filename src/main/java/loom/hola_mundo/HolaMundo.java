e969d51b416e38d5a3236fd0439c7791b725659a
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-23 15:04:18 -0300

    Mejora y adecuación de comentarios

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,24 +1,28 @@
 package loom.hola_mundo;
 
-/*
+/**
  * @author lunavi97
- * Devuelve el saludo a una persona en particular o de manera genérica si no se indica.
+ * Clase HolaMundo.
+ * Devuelve el saludo a una persona en particular o
+ * de manera genérica si no se indica.
  */
 public class HolaMundo {
 
-	/*
+	/**
 	 * Saludar.
-	 * 
 	 * @param nombre de la persona a saludar.
 	 * @return mensaje de saludo.
 	 */
 	public String saludar(final String persona) {
-		return persona == "" ? "¡Hola!" : "¡Hola, " + persona.trim() + "!";
+		if(persona == "") {
+			return "¡Hola!";
+		} else {
+			return "¡Hola, " + persona.trim() + "!";
+		}
 	}
 
-	/*
+	/**
 	 * Saludar.
-	 * 
 	 * @return saludo genérico.
 	 */
 	public String saludar() {
